0f570c3c7f24a6fb3707eb3bee6cfba351878b20
Rodrigo Kraus <rodrikraus10@hotmail.com>
2025-11-20 19:04:29 -0300

    Init.

---
+++ microservice-product/src/main/java/com/microservice/Models/Product.java
@@ -0,0 +1,29 @@
+package com.microservice.Models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "productos")
+public class Product {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column (name = "name", nullable = false)
+    private String name;
+    private BigDecimal price;
+    private Integer stock;
+    @Column(length = 1024)
+    private String description;
+    @Column(length = 512)
+    private String imageUrl;
+    @Column(length = 256)
+    private String tags;
+    private String productCategory;
+}
---
+++ microservice-product/src/main/java/com/microservice/Repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.microservice.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.microservice.Models.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {}
